09115eb738841b2692dd05abd09759c4d91dba3c
Gregor Wolf <gregor.wolf@gmail.com>
2016-07-10 08:53:15 +0000

    Fix unit test for formatter

--- webapp/test/unit/model/formatter.js
+++ webapp/test/unit/model/formatter.js
@@ -1,38 +1,39 @@
 sap.ui.define([
-	"sap/m/Text",
-	"com/sap/sapmentors/sitreg/events/model/formatter"
-], function(Text, formatter) {
-	"use strict";
+		"com/sap/sapmentors/sitreg/events/model/formatter",
+		"sap/ui/thirdparty/sinon",
+		"sap/ui/thirdparty/sinon-qunit"
+	], function (formatter) {
+		"use strict";
 
-	QUnit.module("formatter - Currency value");
+		QUnit.module("Number unit");
 
-	function currencyValueTestCase(, sValue, fExpectedNumber) {
-		// Act
-		var fCurrency = formatter.currencyValue(sValue);
+		function numberUnitValueTestCase(assert, sValue, fExpectedNumber) {
+			// Act
+			var fNumber = formatter.currencyValue(sValue);
 
-		// 
+			// Assert
+			assert.strictEqual(fNumber, fExpectedNumber, "The rounding was correct");
+		}
 
-		.strictEqual(fCurrency, fExpectedNumber, "The rounding was correct");
+		QUnit.test("Should round down a 3 digit number", function (assert) {
+			numberUnitValueTestCase.call(this, assert, "3.123", "3.12");
+		});
+
+		QUnit.test("Should round up a 3 digit number", function (assert) {
+			numberUnitValueTestCase.call(this, assert, "3.128", "3.13");
+		});
+
+		QUnit.test("Should round a negative number", function (assert) {
+			numberUnitValueTestCase.call(this, assert, "-3", "-3.00");
+		});
+
+		QUnit.test("Should round an empty string", function (assert) {
+			numberUnitValueTestCase.call(this, assert, "", "");
+		});
+
+		QUnit.test("Should round a zero", function (assert) {
+			numberUnitValueTestCase.call(this, assert, "0", "0.00");
+		});
+		
 	}
-
-	QUnit.test("Should round down a 3 digit number", function() {
-		currencyValueTestCase.call(this, , "3.123", "3.12");
-	});
-
-	QUnit.test("Should round up a 3 digit number", function() {
-		currencyValueTestCase.call(this, , "3.128", "3.13");
-	});
-
-	QUnit.test("Should round a negative number", function() {
-		currencyValueTestCase.call(this, , "-3", "-3.00");
-	});
-
-	QUnit.test("Should round an empty string", function() {
-		currencyValueTestCase.call(this, , "", "");
-	});
-
-	QUnit.test("Should round a zero", function() {
-		currencyValueTestCase.call(this, , "0", "0.00");
-	});
-
-});
+);
