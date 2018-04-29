d51af9ef56e9edfb30d02f8099ff27fcb780fdbe
gregorwolf <gregor.wolf@gmail.com>
2018-04-29 15:09:33 +0000

    Updated UI5 build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,9 @@
-module.exports = function (grunt) {
-    'use strict';
-    grunt.loadNpmTasks('@sap/grunt-sapui5-bestpractice-build');
-
-    grunt.registerTask('default', [
-        'lint',
-        'clean',
-        'build'
-    ]);
+module.exports = function(grunt) {
+            "use strict";
+            grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
+            grunt.registerTask("default", [
+                        "clean",
+                        "lint",
+                        "build"
+            ]);
 };
