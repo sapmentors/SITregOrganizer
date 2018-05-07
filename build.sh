8983ffd8275f2e9d31ca8d88a9162b1cf578920c
Gregor Wolf <gregor.wolf@gmail.com>
2018-05-07 13:19:33 +0200

    Install MTA tool, build

---
+++ build.sh
@@ -0,0 +1,23 @@
+# install the MTA archive builder
+WORKSPACE=`pwd`
+
+# install neo command line client
+mkdir -p ${WORKSPACE}/tmp/neo-java-web-sdk
+cd ${WORKSPACE}/tmp/neo-java-web-sdk
+wget -nv 'http://central.maven.org/maven2/com/sap/cloud/neo-java-web-sdk/3.52.15/neo-java-web-sdk-3.52.15.zip'
+unzip -qq -o neo-java-web-sdk-3.52.15.zip
+rm neo-java-web-sdk-3.52.15.zip
+
+# extract artifact name
+cd ${WORKSPACE}
+wget -nv --output-document=jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+chmod +x ./jq
+mtaName=`js-yaml mta.yaml | jq -r '.ID'`
+
+# replace timestamp placeholder
+sed -ie "s/\${timestamp}/`date +%Y%m%d%H%M%S`/g" mta.yaml
+
+# execute MTA build
+java -jar ${WORKSPACE}/tmp/mta/mta.jar --mtar ${CIRCLE_PROJECT_REPONAME}.mtar --build-target=NEO build
+#mkdir -p ${WORKSPACE}/dist/mta
+#cp ${CIRCLE_PROJECT_REPONAME}.mtar dist/mta/${CIRCLE_PROJECT_REPONAME}-`date +%Y%m%d%H%M%S`.mtar
