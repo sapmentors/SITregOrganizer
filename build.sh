fbd97972e3deab12197bf4a2a9456ccffd9b06bf
gregorwolf <gregor.wolf@gmail.com>
2018-12-16 09:55:35 +0000

    Updated SAP CP SDK

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ WORKSPACE=`pwd`
 # install neo command line client
 mkdir -p ${WORKSPACE}/tmp/neo-java-web-sdk
 cd ${WORKSPACE}/tmp/neo-java-web-sdk
-wget -nv 'http://central.maven.org/maven2/com/sap/cloud/neo-java-web-sdk/3.52.15/neo-java-web-sdk-3.52.15.zip'
+wget -nv 'http://central.maven.org/maven2/com/sap/cloud/neo-java-web-sdk/3.68.11/neo-java-web-sdk-3.68.11.zip'
 unzip -qq -o neo-java-web-sdk-3.52.15.zip
 rm neo-java-web-sdk-3.52.15.zip
 
