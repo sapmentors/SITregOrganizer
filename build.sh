2f4b10961ccf541c27234f7caee44a906ac72252
Gregor Wolf <gregor.wolf@gmail.com>
2020-02-26 09:23:34 +0100

    Update dependencies

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # download the MTA archive builder
 mkdir -p tmp/mta
-wget -nv --output-document=tmp/mta/mta.jar --no-cookies --header "Cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt" https://tools.hana.ondemand.com/additional/mta_archive_builder-1.1.19.jar
+wget -nv --output-document=tmp/mta/mta.jar --no-cookies --header "Cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt" https://tools.hana.ondemand.com/additional/mta_archive_builder-1.1.20.jar
 
 # install the MTA archive builder
 WORKSPACE=`pwd`
@@ -8,14 +8,14 @@ WORKSPACE=`pwd`
 # install neo command line client
 mkdir -p ${WORKSPACE}/tmp/neo-java-web-sdk
 cd ${WORKSPACE}/tmp/neo-java-web-sdk
-export neosdk=3.81.16
-wget -nv "http://central.maven.org/maven2/com/sap/cloud/neo-java-web-sdk/${neosdk}/neo-java-web-sdk-${neosdk}.zip"
+export neosdk=3.99.14
+wget -nv "https://repo1.maven.org/maven2/com/sap/cloud/neo-java-web-sdk/${neosdk}/neo-java-web-sdk-${neosdk}.zip"
 unzip -qq -o neo-java-web-sdk-${neosdk}.zip
 rm neo-java-web-sdk-${neosdk}.zip
 
 # extract artifact name
 cd ${WORKSPACE}
-wget -nv --output-document=jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+wget -nv --output-document=jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 mtaName=`js-yaml mta.yaml | ./jq -r '.ID'`
 
